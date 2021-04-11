4c268e87b46de92672f503789e7dd5fec393982d
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2021-04-10 19:52:09 -0500

    el cliente del segundo jugador listo, en eclipse

---
+++ src/main/Coordenada.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Coordenada {
+
+	//Atributos
+	private String accion;
+	
+	//Constructores
+	public Coordenada(String accion) {
+		this.accion = accion;
+	}
+
+	public String getAccion() {
+		return accion;
+	}
+
+	public void setAccion(String accion) {
+		this.accion = accion;
+	}
+
+
+	
+	
+}
